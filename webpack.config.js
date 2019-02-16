72dc5230ab4ace0d7d35cfd202c456b4fe3b022b
tyshgc <hello@tyshgc.pw>
2019-02-16 19:30:37 +0900

    Enhanced: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,46 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CheckerPlugin } = require("awesome-typescript-loader");
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CheckerPlugin } = require("awesome-typescript-loader")
 
 module.exports = {
   entry: "./src/app.tsx",
   output: {
     filename: "bundle.js",
-    path: __dirname + "/dist"
+    path: __dirname + "/dist",
   },
   devtool: "source-map",
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"]
+    extensions: [".ts", ".tsx", ".js", ".json"],
   },
 
   module: {
     rules: [
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-    ]
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)(\?.+)?$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 8192,
+            name: "./assets/[name].[ext]",
+          },
+        },
+      },
+    ],
   },
   devServer: {
     contentBase: path.join(__dirname, "public"),
     watchContentBase: true,
-    open: true
+    open: true,
   },
   optimization: {
     splitChunks: {
-      chunks: "all"
-    }
+      chunks: "all",
+    },
   },
   plugins: [
     new CheckerPlugin(),
-    new HtmlWebpackPlugin({ template: "src/index.html" })
-  ]
-};
+    new HtmlWebpackPlugin({ template: "src/index.html" }),
+  ],
+}
